b3f3ba6f2c376b25e47902cc9d9930c72cba4d5e
farhanakram1 <farhanakram670@gmail.com>
2020-06-23 16:50:59 +0500

    Style Changes

--- enrol/renderer.php
+++ enrol/renderer.php
@@ -658,7 +658,7 @@ class course_enrolment_other_users_table extends course_enrolment_table {
      */
     public function __construct(course_enrolment_manager $manager) {
         parent::__construct($manager);
-        $this->attributes = array('class'=>'userenrolment otheruserenrolment');
+        $this->attributes = array('class'=>'userenrolment otheruserenrolment table-responsive');
     }
 
     /**
