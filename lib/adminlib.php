b8b0a48cd615c4fcef57faadc26625e0c961b179
farhanakram1 <farhanakram670@gmail.com>
2020-07-06 16:07:41 +0500

    Style Changes

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -9427,12 +9427,12 @@ class admin_setting_manageexternalservices extends admin_setting {
         // Custom services
         $return .= $OUTPUT->heading(get_string('servicescustom', 'webservice'), 3, 'main');
         $services = $DB->get_records_select('external_services', 'component IS NULL', null, 'name');
-        html_writer::start_tag('div', array('class' => 'table-responsive'));
+
         $table = new html_table();
         $table->head  = array($strservice, $strdelete, $strfunctions, $strusers, $stredit);
         $table->colclasses = array('leftalign service', 'leftalign plugin', 'centeralign functions', 'centeralign users', 'centeralign ');
         $table->id = 'customservices';
-        $table->attributes['class'] = 'admintable externalservices generaltable';
+        $table->attributes['class'] = 'admintable externalservices generaltable table-responsive';
         $table->data  = array();
 
         // iterate through auth plugins and add to the display table
@@ -9465,7 +9465,6 @@ class admin_setting_manageexternalservices extends admin_setting {
         // add new custom service option
         $return .= html_writer::table($table);
 
-        $return .= html_writer::end_tag('div');;
         $return .= '<br />';
         // add a token to the table
         $return .= "<a href=\"$esurl?id=0\">$stradd</a>";
